d6f6cb89da8a0ba20a25f089d776ef042a2b0259
kimberly malda <maldakimberly228@gmail.com>
2025-05-09 13:38:22 +0300

    completed sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,59 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+    
+    print(sequence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#name = "Stella"
+#print(name[1])
+
+#class Dog:
+   # def __init__(self, name, breed):
+       # self.name = name
+       # self.breed = breed
+
+    #def bark(self):
+       # return "Woof!"
+
+#my_dog = Dog("Buddy", "Golden Retriever")
+#print(my_dog.name)  
+#print(my_dog.bark())  
+
+# def __init__(self,name,color,age):
+       # self.name = name
+        #self.color = color
+        #self.age = age
+
+   # def announceOwner(self,owner) :
+        #print(owner)
+        #print(self.name)
+        #print(self.color)
+
+        #Daisy = Pet("Daisy","white",1)
+        #Daisy.announceOwner("Ike")
+        
